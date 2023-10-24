089789ded13b16e02819ae5ee36fe822bb2d1328
Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>
2023-10-23 23:38:27 -0300

    Alteração para exibir o swagger

--- src/1-Presentation/1.1-API/TechChallenge.AutomotiveMechanics.Presentation.API/Program.cs
+++ src/1-Presentation/1.1-API/TechChallenge.AutomotiveMechanics.Presentation.API/Program.cs
@@ -23,11 +23,12 @@ builder.Services.AddInfrastructureConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+
+//}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseReDoc(c =>
 {

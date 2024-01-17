e6d72f3f7fa1001edf86971b7fa663c69215cdef
João Paulo <49122534+jpmarques2000@users.noreply.github.com>
2024-01-17 10:05:09 -0300

    Adição de integração com azure durable function Approval

---
+++ src/1-Presentation/1.1-API/TechChallenge.AutomotiveMechanics.Presentation.API/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using TechChallenge.AutomotiveMechanics.Services.Business.Input;
+using TechChallenge.AutomotiveMechanics.Services.Business.Interfaces.Services;
+
+namespace TechChallenge.AutomotiveMechanics.Presentation.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrderController : BaseController
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public OrderController(IBaseNotification baseNotification,
+            IHttpClientFactory httpClientFactory)
+            : base(baseNotification)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PlaceOrder(OrderInsertInput order)
+        {
+            var azFunctionUrl = "https://az-orderapproval.azurewebsites.net/api/HttpStart_OrderApproval?";
+
+            using(var httpClient = _httpClientFactory.CreateClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(order), Encoding.UTF8, "application/json");
+
+                var response = await httpClient.PostAsync(azFunctionUrl, content);
+
+                return CreatedOrBadRequest(response);
+            }
+        }
+    }
+}

aca7933cf072e1ab68fe86d6d99e587e47cda468
João Paulo <49122534+jpmarques2000@users.noreply.github.com>
2024-03-07 16:39:10 -0300

    add email service (#26)

---
+++ src/2-Services/2.1-Business/TechChallenge.AutomotiveMechanics.Services.Business/Services/EmailService.cs
@@ -0,0 +1,40 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using MimeKit.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechChallenge.AutomotiveMechanics.Services.Business.Input;
+using TechChallenge.AutomotiveMechanics.Services.Business.Interfaces.Services;
+
+namespace TechChallenge.AutomotiveMechanics.Services.Business.Services
+{
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void SendEmail(EmailTemplate request)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(_config.GetSection("EmailUsername").Value));
+            email.To.Add(MailboxAddress.Parse(request.SendTo));
+            email.Subject = request.Subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = request.Body };
+
+            using var smtp = new SmtpClient();
+            smtp.Connect(_config.GetSection("EmailHost").Value, 587, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_config.GetSection("EmailUsername").Value, _config.GetSection("EmailPassword").Value);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}

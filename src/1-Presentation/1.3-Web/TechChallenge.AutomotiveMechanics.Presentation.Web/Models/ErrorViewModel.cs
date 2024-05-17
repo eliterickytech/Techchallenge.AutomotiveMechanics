bbc566263e8b5aeec6eedcf90bb9e6f61af4ae64
Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>
2024-05-17 20:18:24 -0300

    Delete src/1-Presentation/1.3-Web/TechChallenge.AutomotiveMechanics.Presentation.Web directory
    Deletar
    
    Signed-off-by: Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>

--- src/1-Presentation/1.3-Web/TechChallenge.AutomotiveMechanics.Presentation.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TechChallenge.AutomotiveMechanics.Presentation.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- src/1-Presentation/1.3-Web/TechChallenge.AutomotiveMechanics.Presentation.Web/Models/UserModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TechChallenge.AutomotiveMechanics.Presentation.Web.Models
-{
-    public class UserModel : EntityModel
-    {
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}

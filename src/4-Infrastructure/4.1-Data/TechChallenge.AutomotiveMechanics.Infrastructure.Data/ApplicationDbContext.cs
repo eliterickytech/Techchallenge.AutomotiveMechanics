ede87aac7ca134e3ca2f7eff8b7e489e4951672a
jpmarques2000 <jpmarques2000@hotmail.com>
2024-05-19 16:03:48 -0300

    Ajustes develop

--- src/4-Infrastructure/4.1-Data/TechChallenge.AutomotiveMechanics.Infrastructure.Data/ApplicationDbContext.cs
+++ src/4-Infrastructure/4.1-Data/TechChallenge.AutomotiveMechanics.Infrastructure.Data/ApplicationDbContext.cs
@@ -24,9 +24,6 @@ namespace TechChallenge.AutomotiveMechanics.Infrastructure.Data
 
         public DbSet<User> Users { get; set; }
 
-        public DbSet<Order> Orders { get; set; }
-
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
